28234bd006aa52d4f5c72a857655db420b7a7702
scabongamedev-code <scabon.game.dev@gmail.com>
2026-09-09 12:27:47 +0300

    add key

---
+++ scripts/the_key.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		change_to_next_level()
+
+
+func change_to_next_level():
+	print("ok goining to the next level")

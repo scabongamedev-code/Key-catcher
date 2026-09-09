cae2f6e3815a1c9fc1c46d0140f9ed2daad3e0fe
scabongamedev-code <scabon.game.dev@gmail.com>
2026-09-09 12:20:10 +0300

    Add Player

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+@export var current_speed: float = 300.0
+@export var jump_force: float = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+
+	move_and_slide()
